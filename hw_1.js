eb53eed2e4b04bade552c405ff79399db669a446
Daria Chikovani <sheburshak@yandex.ru>
2022-05-23 05:56:09 +0300

    add new line in file hw_1.js

--- hw_1.js
+++ hw_1.js
@@ -126,3 +126,35 @@ checkAge (18)
 checkAge (61)
 
 
+//2*:
+//Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных. И если он не Number - кидалась ошибка.
+
+   let age = 17;
+   let age_2 = 18;
+   let age_3 = 61;
+
+   function chekcAge(age) {
+    
+    if (typeof age === "number") {
+          if (age < age_2) {
+            console.log("You dont have access cause your age is " + age_1 + " Its less then")
+        } 
+        else if (age >= age_2 && age < age_3) {
+            console.log("Welcome!")  
+        }
+         else if (age > age_3) {
+            console.log("Keep calm and look Culture channel")
+        }
+         else {
+            console.log("Technical work")
+        }
+    }
+   else {
+       console.log("Error! wrong data type")
+   } 
+
+   } 
+      
+chekcAge (18)
+
+
