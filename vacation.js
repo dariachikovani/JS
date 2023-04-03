e90891f64b014d7ce71c6efcc5724df38ae9c857
Daria Chikovani <sheburshak@yandex.ru>
2023-04-03 20:58:08 +0300

    Add new file vacation.js

---
+++ vacation.js
@@ -0,0 +1,12 @@
+let vacation = "You'r vacation approved";
+let denied = "You'r vacation denied";
+
+let response = prompt("Enter a month");
+
+if (response == "July" || response == "August"){
+  alert(vacation)
+}
+else{
+  alert(denied)
+}
+
