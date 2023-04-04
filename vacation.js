e4166bccdd1e930096dd0f3260c74a575aa2d5dd
Daria Chikovani <sheburshak@yandex.ru>
2023-04-05 01:02:53 +0300

    Add new lines in  file vacation.js

--- vacation.js
+++ vacation.js
@@ -1,3 +1,6 @@
+//1.1 Напишите программу по согласованию отпуска в зависимости от месяца. Если месяц “July” или “August”, то программа должна показать - ‘approved’, если другие месяцы, то - “denied”.
+
+
 let vacation = "You'r vacation approved";
 let denied = "You'r vacation denied";
 
